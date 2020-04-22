68886e42592f19ee32c142c19b76785944d98945
SayaliTanawade <tanawade.sayali@gmail.com>
2020-04-22 16:14:08 -0700

    initial commit for main compiler classes

---
+++ src/sparkyCompiler/Compiler.java
@@ -0,0 +1,59 @@
+package sparkyCompiler;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import sparky.SparkyLexer;
+import sparky.SparkyParser;
+
+public class Compiler {
+
+	public static void main(String[] args) {
+		try {
+			
+			String inputFileName = "data/Example 1.sparky";
+			//String iCodeFilename;
+    		
+    		CharStream sourceCode = CharStreams.fromFileName(inputFileName);
+    		SparkyLexer lx = new SparkyLexer(sourceCode);
+    		CommonTokenStream tokenStream = new CommonTokenStream(lx);
+    		SparkyParser parser = new SparkyParser(tokenStream);
+    		ParseTree parseTree = parser.program();
+    		
+    		
+    		//iCodeFilename = inputFileName.split("\\.")[0] + ".isparky";
+    		IntermediateCodeGenerator iCodeGen = new IntermediateCodeGenerator();
+    		iCodeGen.visit(parseTree);
+    		
+    		List<String> iCodeArray = Arrays.asList(iCodeGen.getOutput().split("\\n"));
+//    		if(iCodeArray.size()>1)
+//    		{
+    	       
+				PrintWriter writer = new PrintWriter(inputFileName.replace("sparky", "sparkyic"), "UTF-8");
+    			
+    			for(int i=0; i<iCodeArray.size();i++)
+    			{
+    				writer.println(iCodeArray.get(i));
+    			}
+    			writer.close();
+    			
+    			//File iCodeFile = new File(iCodeFilename);
+    		//}
+
+        
+	}
+	catch (Exception e)
+	{
+		System.out.println("Wrong Input file");
+	}
+
+		}
+
+	}
---
+++ src/sparkyCompiler/IntermediateCodeGenerator.java
@@ -0,0 +1,178 @@
+package sparkyCompiler;
+
+
+/**
+ * In this class methods from SparkyBaseVisitor are overloaded for intermediate course generation according to Sparky grammar.
+ * @author Sayali Tanawade
+ * @since April-20-2020
+ * @version 1.0
+ */
+
+import sparky.SparkyBaseVisitor;
+import sparky.SparkyParser;
+
+public class IntermediateCodeGenerator extends SparkyBaseVisitor<Object> {
+	
+	private class Helper {
+        private String icOutput = "";
+        private void addOutput(String output) {
+            this.icOutput += output + "\n";
+        }
+    }
+	
+	private Helper help = new Helper();
+	
+	public String getOutput() {
+		return help.icOutput;
+	}
+	
+	@Override public Object visitProgram(SparkyParser.ProgramContext ctx) { return visitChildren(ctx); }
+	
+	@Override public Object visitBall(SparkyParser.BallContext ctx) { return visitChildren(ctx); }
+	
+	@Override public Object visitDeclare(SparkyParser.DeclareContext ctx) {
+		String identifier = ctx.STUFF().getText();
+		help.addOutput(identifier);
+		
+		return null; 
+		
+	}
+	
+	@Override public Object visitExpression(SparkyParser.ExpressionContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitAssignment(SparkyParser.AssignmentContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitIfte(SparkyParser.IfteContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitLoopum(SparkyParser.LoopumContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitLoop_for(SparkyParser.Loop_forContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitLoop_while(SparkyParser.Loop_whileContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitIn_loop(SparkyParser.In_loopContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitFor_expr(SparkyParser.For_exprContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitFor_expression(SparkyParser.For_expressionContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitFor_declare(SparkyParser.For_declareContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitTerm(SparkyParser.TermContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitExpr(SparkyParser.ExprContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitYesnostatement(SparkyParser.YesnostatementContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitTernary_operator(SparkyParser.Ternary_operatorContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitPrint(SparkyParser.PrintContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitWarna(SparkyParser.WarnaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitHaina(SparkyParser.HainaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitDatatype(SparkyParser.DatatypeContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitYup(SparkyParser.YupContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public Object visitNope(SparkyParser.NopeContext ctx) { return visitChildren(ctx); }
+	
+		
+
+}
