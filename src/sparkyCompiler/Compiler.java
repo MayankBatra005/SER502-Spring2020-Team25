ec09cb55bb51153c98727b415eccb438a6c8ab5b
SayaliTanawade <tanawade.sayali@gmail.com>
2020-04-26 20:18:50 -0700

    Updated error conditions for file read

--- src/sparkyCompiler/Compiler.java
+++ src/sparkyCompiler/Compiler.java
@@ -1,7 +1,9 @@
 package sparkyCompiler;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,45 +19,68 @@ public class Compiler {
 
 	public static void main(String[] args) {
 		try {
-			
-			String inputFileName = "data/print.sparky";
-			
-			//String iCodeFilename;
-    		
-    		CharStream sourceCode = CharStreams.fromFileName(inputFileName);
-    		SparkyLexer lx = new SparkyLexer(sourceCode);
-    		CommonTokenStream tokenStream = new CommonTokenStream(lx);
-    		SparkyParser parser = new SparkyParser(tokenStream);
-    		ParseTree parseTree = parser.program();
-    		
-    		
-    		//iCodeFilename = inputFileName.split("\\.")[0] + ".isparky";
-    		IntermediateCodeGenerator iCodeGen = new IntermediateCodeGenerator();
-    		iCodeGen.visit(parseTree);
-    		
-    		List<String> iCodeArray = Arrays.asList(iCodeGen.getOutput().split("\\n"));
-//    		if(iCodeArray.size()>1)
-//    		{
-    	       
-				PrintWriter writer = new PrintWriter(inputFileName.replace("sparky", "sparkyic"), "UTF-8");
-    			
-    			for(int i=0; i<iCodeArray.size();i++)
-    			{
-    				writer.println(iCodeArray.get(i));
-    			}
-    			writer.close();
-    			
-    			//File iCodeFile = new File(iCodeFilename);
-    		//}
 
-        
-	}
-	catch (Exception e)
-	{
-//		System.out.println("Wrong Input file" + e.p);
-		e.printStackTrace();
-	}
+			if (args.length > 0) {
+				String inputFileName = args[0];
 
+			 //String inputFileName = "data/print.sparky";
+	
+			CharStream sourceCode = CharStreams.fromFileName(inputFileName);
+			SparkyLexer lx = new SparkyLexer(sourceCode);
+			CommonTokenStream tokenStream = new CommonTokenStream(lx);
+			SparkyParser parser = new SparkyParser(tokenStream);
+			ParseTree parseTree = parser.program();
+
+			IntermediateCodeGenerator iCodeGen = new IntermediateCodeGenerator();
+			iCodeGen.visit(parseTree);
+
+			List<String> iCodeArray = Arrays.asList(iCodeGen.getOutput().split("\\n"));
+
+			try {
+
+				File iCodeFile = new File(inputFileName);
+				BufferedWriter bufferWriter = null;
+				FileWriter fileWriter = null;
+				if (iCodeArray.size() > 1) {
+					try {
+						if (iCodeFile.exists()) {
+							iCodeFile.delete();
+							iCodeFile.createNewFile();
+						} else {
+							iCodeFile.createNewFile();
+						}
+
+						fileWriter = new FileWriter(inputFileName.replace("sparky", "sparkyic"));
+						bufferWriter = new BufferedWriter(fileWriter);
+						for(int i=0; i<iCodeArray.size();i++){
+							bufferWriter.write(iCodeArray.get(i)+ " " + "\n");
+						}
+						
+					} catch (IOException e) {
+						e.printStackTrace();
+
+					} finally {
+						try {
+							if (bufferWriter != null)
+								bufferWriter.close();
+							if (fileWriter != null)
+								fileWriter.close();
+
+						} catch (IOException ex) {
+							ex.printStackTrace();
+						}
+					}
+				}
+			}
+
+			catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
+		} catch (Exception e) {
+		System.out.println("Input filename is incorrect");
+			e.printStackTrace();
+		}
+	}
 
-}
+}	
--- src/sparkyRuntime/Main.java
+++ src/sparkyRuntime/Main.java
@@ -1,3 +1,4 @@
+package sparkyRuntime;
 
 public class Main {
 	public static void main(String[] str) throws Exception {
