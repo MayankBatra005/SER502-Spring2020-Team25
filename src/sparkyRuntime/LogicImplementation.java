cbd29a2f8d3ddae60fcb8822f5ae16e6b7fcc391
MayankBatra005 <er.mayank005@gmail.com>
2020-04-28 09:32:21 -0700

    Implementing Design pattern to prevent data corruption

--- src/sparkyRuntime/LogicImplementation.java
+++ src/sparkyRuntime/LogicImplementation.java
@@ -7,49 +7,52 @@ import java.util.HashMap;
 import java.util.Stack;
 
 /**
- * @author Mayank PC
+ * @author Mayank Batra
  *
+ *         Singleton pattern implemented to avoid any corruption of data
  */
 public class LogicImplementation {
-	
+
+	public static LogicImplementation singeltonInstance;
+
+	private LogicImplementation() {
+	}
+
+	public static LogicImplementation getInstance() {
+		if (singeltonInstance == null) {
+			singeltonInstance = new LogicImplementation();
+		}
+		return singeltonInstance;
+	}
+
 	HashMap<String, DataTypes> map = new HashMap<String, DataTypes>();
 	Stack<DataTypes> local = new Stack<DataTypes>();
 	Stack<String> forVariable = new Stack<String>();
 	int counter = 0;
 	Stack<String> whileVariable = new Stack<String>();
-	
-	public void declareLogic()
-	{
-		
-		
+
+	public void declareLogic() {
+
 	}
-	
-	public void storeLogic()
-	{
-		
-		
+
+	public void storeLogic() {
+
 	}
-	
-	public void getLogic()
-	{
-		
+
+	public void getLogic() {
+
 	}
-	
-	public void whileLogic()
-	{
-		
+
+	public void whileLogic() {
+
 	}
-	
-	public void forLogic()
-	{
-		
+
+	public void forLogic() {
+
 	}
-	
-	public void ifThenElseLogic()
-	{
-		
+
+	public void ifThenElseLogic() {
+
 	}
-	
-	
 
 }
